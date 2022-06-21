8729b0cb2cd4fcd103e2e8261497c05ec19b3667
nachelysanchez <nashely-5@hotmail.com>
2022-06-20 23:36:19 -0400

    Creando clase Exchange

--- app/src/main/java/com/example/retrofitapi/data/remote/dto/Exchange.kt
+++ app/src/main/java/com/example/retrofitapi/data/remote/dto/Exchange.kt
@@ -1,9 +1,9 @@
 package com.example.retrofitapi.data.remote.dto
 
-data class Exchanges (
+data class Exchange(
     val exchangeId : String = "",
     val name : String = "",
     val description : String = "",
     val active : Boolean = false,
-    val last_update : String = ""
+    val last_updated : String = ""
 )
--- app/src/main/java/com/example/retrofitapi/data/remote/dto/ExchangeListState.kt
+++
@@ -1,7 +0,0 @@
-package com.example.retrofitapi.data.remote.dto
-
-data class ExchangeListState (
-        val isLoading : Boolean = false,
-        val exchanges : List<Exchanges> = emptyList(),
-        val error : String = ""
-)
